a0aff771f0b4d1f071cf06bb604442b4ac4e43a7
Doug Wenzler <44591831+wenzler15@users.noreply.github.com>
2020-03-25 14:31:47 -0300

    aula terça

---
+++ backend/src/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+/*
+    Tipos de parâmetros:
+
+    Query Params: Parâmetros nomeados enviados na após "?" (Filtros, paginação)
+    Routes Parms: Parâmetros utilizados para identificar recursos
+    Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
